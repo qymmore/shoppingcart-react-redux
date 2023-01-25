deed0b9f0294f69589085dfdbdc5e4858d7d3fc5
Sarah Quayyum <95451775+qymmore@users.noreply.github.com>
2023-01-24 20:01:38 -0500

    add tailwindcss to streamline UI design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './index.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-  
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+function Home(){
+    return(
+        <h1 className='bg-indigo-400'>Hello world</h1>
+    )
+}
+
+export default Home;
